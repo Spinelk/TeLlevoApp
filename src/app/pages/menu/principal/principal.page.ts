0a11924218cd01f0cf1242ed5cf48dfa573a8c75
Kevin <ke.espinel@gmail.com>
2023-09-07 23:12:20 -0300

    Se agrega animación

--- src/app/pages/menu/principal/principal.page.ts
+++ src/app/pages/menu/principal/principal.page.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Usuario } from 'src/app/models/usuario';
 import { AlertService } from 'src/app/services/global/alert.service';
 import { ConductoresService } from 'src/app/services/login/conductores.service';
 import { UsuariosService } from 'src/app/services/login/usuarios.service';
+import { IonCard , AnimationController} from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-principal',
@@ -15,13 +17,28 @@ export class PrincipalPage implements OnInit {
   usuario: any;
   tipo: number = 0;
 
+  @ViewChild(IonCard, { read: ElementRef }) card!: ElementRef<HTMLIonCardElement>;
+  private animation!: Animation;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private usuarioService: UsuariosService,
     private conductorService: ConductoresService,
     private alertService: AlertService,
+    private animationCtrl: AnimationController
   ) { }
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(document.querySelectorAll("#cardOne"))
+      .duration(2000)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(100px)', 'translateX(-100px)')
+      .fromTo('opacity', '1', '0.2');
+    this.play();
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -34,7 +51,13 @@ export class PrincipalPage implements OnInit {
       }
       this.tipo = tipo;
     });
+    
   }
+
+  play(){
+    this.animation.play();
+  }
+
 
   irAInicio() {
     this.router.navigateByUrl("inicio-sesion");
@@ -48,8 +71,12 @@ export class PrincipalPage implements OnInit {
     this.router.navigate(['/vehiculo', this.usuario.correo]);
   }
 
-  cerrarSesion() {
-    this.alertService.showAlert("Vuelve pronto.", "Sesión Finalizada");
-    this.irAInicio();
+  async cerrarSesion() {
+    let confirm = await this.alertService.showConfirm("¿Está seguro que desea cerrar sesión?", "Si", "No");
+    if (confirm) {  
+
+      this.alertService.showAlert("Vuelve pronto.", "Sesión Finalizada");
+      this.irAInicio();
+    }
   }
 }
