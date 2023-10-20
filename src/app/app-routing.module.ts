d3dade0127b0798f577e3e3697875ae9c4d07e9f
Kevin <ke.espinel@gmail.com>
2023-10-20 17:33:24 -0300

    refactorizacion a app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,70 +1,79 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard'
-const redireccionarlogin = () => redirectUnauthorizedTo(['/inicio-sesion']);
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/compat/auth-guard'
+const redireccionarPrincipal = () => redirectLoggedInTo(['principal']);
+const redireccionarLogin = () => redirectUnauthorizedTo(['inicio-sesion']);
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'principal',
     pathMatch: 'full'
   },
+
   {
-    path: 'inicio-sesion',
-    loadChildren: () => import('./pages/login/inicio-sesion/inicio-sesion.module').then( m => m.InicioSesionPageModule)
-  },
-  {
-    path: 'registro-usuario',
-    loadChildren: () => import('./pages/login/registro-usuario/registro-usuario.module').then( m => m.RegistroUsuarioPageModule)
-  },
-  {
-    path: 'principal',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin},
-    loadChildren: () => import('./pages/menu/principal/principal.module').then( m => m.PrincipalPageModule)
-  },
-  {
-    path: 'solicitar-viaje',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin}, //PREGUNTAR! es necesario? o solo menú principal?
-    loadChildren: () => import('./pages/viajero/solicitar-viaje/solicitar-viaje.module').then( m => m.SolicitarViajePageModule)
-  },
-  {
-    path: 'confirmar-solicitud/:id',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin}, //PREGUNTAR! es necesario? o solo menú principal?
-    loadChildren: () => import('./pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.module').then( m => m.ConfirmarPageModule)
-  },
-  {
-    path: 'cambiar-contrasena',
-    loadChildren: () => import('./pages/login/cambiar-contrasena/cambiar-contrasena.module').then( m => m.CambiarContrasenaPageModule)
-  },
-  {
-    path: 'registrar-vehiculo',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin}, //PREGUNTAR! es necesario? o solo menú principal?
-    loadChildren: () => import('./pages/conductor/registrar-vehiculo/registrar-vehiculo.module').then( m => m.RegistrarVehiculoPageModule)
-  },
-  {
-    path: 'registrar-conductor',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin},
-    loadChildren: () => import('./pages/conductor/registrar-conductor/registrar-conductor.module').then( m => m.RegistrarConductorPageModule)
-  },
-  {
-    path: 'perfil',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin},
-    loadChildren: () => import('./pages/menu/perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-  {
-    path: 'vehiculo',
-    canActivate: [AngularFireAuthGuard], data:{authGuardPipe:redireccionarlogin},
-    loadChildren: () => import('./pages/conductor/vehiculo/vehiculo.module').then( m => m.VehiculoPageModule)
+    path: '',
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redireccionarPrincipal },
+    children: [
+      {
+        path: 'inicio-sesion',
+        loadChildren: () => import('./pages/login/inicio-sesion/inicio-sesion.module').then(m => m.InicioSesionPageModule)
+      },
+      {
+        path: 'registro-usuario',
+        loadChildren: () => import('./pages/login/registro-usuario/registro-usuario.module').then(m => m.RegistroUsuarioPageModule)
+      },
+      {
+        path: 'cambiar-contrasena',
+        loadChildren: () => import('./pages/login/cambiar-contrasena/cambiar-contrasena.module').then(m => m.CambiarContrasenaPageModule)
+      },
+    ]
   },
 
+  {
+    path: '',
+    canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redireccionarLogin },
+    children: [
+      {
+        path: 'principal',
+        loadChildren: () => import('./pages/menu/principal/principal.module').then(m => m.PrincipalPageModule)
+      },
+      {
+        path: 'solicitar-viaje',
+        loadChildren: () => import('./pages/viajero/solicitar-viaje/solicitar-viaje.module').then(m => m.SolicitarViajePageModule)
+      },
+      {
+        path: 'confirmar-solicitud/:id',
+        loadChildren: () => import('./pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.module').then(m => m.ConfirmarPageModule)
+      },
+      {
+        path: 'registrar-vehiculo',
+        loadChildren: () => import('./pages/conductor/registrar-vehiculo/registrar-vehiculo.module').then(m => m.RegistrarVehiculoPageModule)
+      },
+      {
+        path: 'registrar-conductor',
+        loadChildren: () => import('./pages/conductor/registrar-conductor/registrar-conductor.module').then(m => m.RegistrarConductorPageModule)
+      },
+      {
+        path: 'perfil',
+        loadChildren: () => import('./pages/menu/perfil/perfil.module').then(m => m.PerfilPageModule)
+      },
+      {
+        path: 'vehiculo',
+        loadChildren: () => import('./pages/conductor/vehiculo/vehiculo.module').then(m => m.VehiculoPageModule)
+      },
+    ]
+  },
 
-
-
+  {
+    path: '**',
+    redirectTo: 'principal',
+  }
 ];
 
 @NgModule({
