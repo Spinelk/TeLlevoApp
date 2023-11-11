afc2732faf0ac5db2d549ff877dba05e3668ba46
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-11-11 02:55:05 -0300

    Refactor login page HTML and TypeScript code

--- src/app/pages/login/inicio-sesion/inicio-sesion.page.ts
+++ src/app/pages/login/inicio-sesion/inicio-sesion.page.ts
@@ -1,11 +1,8 @@
 // Angular/Ionic
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 // Servicios
-import { ClipboardService } from 'src/app/services/global/clipboard.service';
 import { AutenticacionService } from 'src/app/services/autenticacion/autenticacion.service';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -19,29 +16,11 @@ export class InicioSesionPage {
 
 
   constructor(
-    private router: Router,
-    private clipboardService: ClipboardService,
     private servicioAutenticacion: AutenticacionService,
-    private navController: NavController,
   ) { }
 
-
-  async copiarPortapapeles() {
-    await this.clipboardService.copiarPortapapeles(this.correo);
-  }
-
-
-  iniciar() {
+  iniciarSesion() {
+    console.log(this.correo, this.contrasena)
     this.servicioAutenticacion.iniciarSesion(this.correo, this.contrasena)
-  }
-
-
-  // Navegación. Puede ser reemplazada por un botón en el HTML
-  irARegistro() {
-    // this.navController.setDirection('root');
-    this.router.navigate(['registro-usuario']);
-  }
-  irACambiarContrasena() {
-    this.router.navigate(['cambiar-contrasena']);
   }
 }
