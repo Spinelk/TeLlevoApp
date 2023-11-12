06e0a0bd75345a6a2dcdb2b2e71594d90483af0f
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-11-12 02:45:20 -0300

    Add CompartidosModule and update ConfirmarPage layout

--- src/app/pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.module.ts
+++ src/app/pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { ConfirmarPageRoutingModule } from './confirmar-routing.module';
 
 import { ConfirmarPage } from './confirmar.page';
+import { CompartidosModule } from 'src/app/modules/compartidos/compartidos.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    CompartidosModule,
     ConfirmarPageRoutingModule
   ],
   declarations: [ConfirmarPage]
 })
-export class ConfirmarPageModule {}
+export class ConfirmarPageModule { }
