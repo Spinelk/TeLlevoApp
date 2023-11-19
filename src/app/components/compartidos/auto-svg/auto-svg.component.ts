540d6509e366e543b047a82a3a08773478eaa8fa
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-11-18 22:35:48 -0300

    Add auto-svg component

---
+++ src/app/components/compartidos/auto-svg/auto-svg.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auto-svg',
+  templateUrl: './auto-svg.component.html',
+  styleUrls: ['./auto-svg.component.scss'],
+})
+export class AutoSvgComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
