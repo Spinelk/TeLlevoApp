5aa9dffc330b0eb987be4d96c991218091bf8268
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-11-19 03:03:27 -0300

    Fix vehicle information display in confirmar.page.html and add vehicle retrieval logic in confirmar.page.ts

--- src/app/pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.page.ts
+++ src/app/pages/viajero/solicitar-viaje/confirmar-solicitud/confirmar.page.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { HelperService } from 'src/app/services/global/helper.service';
+import { StorageService } from 'src/app/services/global/storage.service';
 
 @Component({
   selector: 'app-confirmar',
   templateUrl: './confirmar.page.html',
   styleUrls: ['./confirmar.page.scss'],
 })
-export class ConfirmarPage {
+export class ConfirmarPage implements OnInit {
+
+  patente: any;
+  vehiculo: any = {};
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private navController: NavController,
     private alertService: HelperService,
+    private storageService: StorageService,
   ) { }
+
+  ngOnInit() {
+    // Suscribirse a los cambios en los parámetros de la URL
+    this.route.paramMap.subscribe(params => {
+      // Obtener el valor del parámetro de la URL
+      this.patente = params.get('id');
+    });
+
+    this.storageService.obtenerVehiculoPorPatente(this.patente).then((vehiculo) => {
+      this.vehiculo = vehiculo;
+    });
+  }
 
   confirmar() {
     this.alertService.showAlert("Espera la respuesta del conductor", "Viaje Solicitado.");
